5fa8ed2376218835ee5e57bc314ac117c6ee4889
krish-anu <krishnaanu200302@gmail.com>
2025-09-15 17:41:53 +0530

    Image url updated in productVariant in model file

--- backend/models/productVariant.model.js
+++ backend/models/productVariant.model.js
@@ -1,39 +1,46 @@
 module.exports = (sequelize, DataTypes) => {
-    const ProductVariant = sequelize.define("ProductVariant", {
-       id: {
+  const ProductVariant = sequelize.define("ProductVariant", {
+    id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
       allowNull: false,
     },
-        variantName: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-            validate: {
-                notEmpty: true,
-                len: [2, 200]
-            }
-        },
-        SKU: {
-            type: DataTypes.STRING(100),
-            unique: true,
-            allowNull: false,
-            validate: {
-                notEmpty: true,
-                len: [3, 100]
-            }
-        },
-        stockQnt: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1,
-            validate: { min: 0 }
-        },
-        price: {
-            type: DataTypes.DECIMAL(10, 2),
-            allowNull: false,
-            validate: { min: 0 },
-        },
-    });
-    return ProductVariant;
+    variantName: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [2, 200],
+      },
+    },
+    SKU: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [3, 100],
+      },
+    },
+    stockQnt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: { min: 0 },
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: { min: 0 },
+    },
+    imageURL: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+  });
+  return ProductVariant;
 };
